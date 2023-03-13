e4b0abe7d8e6d56162efefc3fd34eb7b3b713df1
Giavinh <giavinhlam@gmail.com>
2023-03-13 17:40:44 -0400

    FEAT-3 Add function return type to getSum (#10)
    * add function return type to getSum
    * minor workflow updates

--- packages/utils/index.ts
+++ packages/utils/index.ts
@@ -2,7 +2,7 @@ export const areNumbersEqual = (a: number, b: number): boolean => {
   return a === b;
 }
 
-export const getSum = (numbers: number[]) => {
+export const getSum = (numbers: number[]): number => {
   return numbers.reduce((acc, num) => {
     return acc;
   }, 0)

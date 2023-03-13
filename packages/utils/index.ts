e68db26a93765b38efb0fa7a3a2d637e49fbb6f3
giavinh79 <giavinhlam@gmail.com>
2023-03-13 03:12:23 -0400

    add sum utility function

--- packages/utils/index.ts
+++ packages/utils/index.ts
@@ -1,3 +1,9 @@
 export const areNumbersEqual = (a: number, b: number): boolean => {
   return a === b;
 }
+
+export const getSum = (numbers: number[]) => {
+  return numbers.reduce((acc, num) => {
+    return acc + num;
+  }, 0)
+}

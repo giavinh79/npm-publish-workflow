e18c627de16279c95aa0a5e1a652370081ce6208
giavinh79 <giavinhlam@gmail.com>
2023-03-14 14:11:11 -0400

    add return type

--- packages/math/index.ts
+++ packages/math/index.ts
@@ -10,6 +10,6 @@ export const multiply = (a: number, b: number): number => {
   return a * b;
 }
 
-export const divide = (a: number, b: number) => {
+export const divide = (a: number, b: number): number => {
   return a / b;
 }

9b3f8bd87497fdc52bb38ddfb0ea5c102ab216f8
giavinh79 <giavinhlam@gmail.com>
2023-03-14 12:46:00 -0400

    test

--- packages/math/index.ts
+++ packages/math/index.ts
@@ -6,7 +6,7 @@ export const subtract = (a: number, b: number): number => {
   return a - b;
 }
 
-export const multiply = (a: number, b: number): number => {
+export const multiply = (a: number, b: number) => {
   return a * b;
 }
 

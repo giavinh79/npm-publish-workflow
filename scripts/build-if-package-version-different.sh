05e3b53e065d3cf1bbf892077c0436bd593a47a9
giavinh79 <giavinhlam@gmail.com>
2023-03-14 03:51:44 -0400

    test

--- scripts/build-if-package-version-different.sh
+++ scripts/build-if-package-version-different.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-if npm diff package.json --silent | grep -q 'version'; then \{ echo | npm diff package.json \} && yarn lint && yarn build; else :; fi
+if npm diff package.json --silent | grep -q 'version'; then yarn lint && yarn build && echo | npm diff package.json; else :; fi

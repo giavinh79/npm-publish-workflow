c124266c0c96f6210e0fb60ed44a7c60bacad388
Giavinh <giavinhlam@gmail.com>
2023-03-14 04:58:15 -0400

    TEST custom script for building only changed packages (#14)
    * custom script for building only changed packages

--- scripts/build-workspace-packages.sh
+++ scripts/build-workspace-packages.sh
@@ -2,6 +2,6 @@
 
 # https://yarnpkg.com/cli/workspaces/foreach
 # investigate another approach using --since prop in yarn workspaces foreach (compare against a git ref to see if a package has been updated)
-# git update-index --chmod=+x your_script.sh to make it executable on CI
+# git update-index --chmod=+x --add your_script.sh to make it executable on CI
 
-yarn workspaces foreach -v --no-private exec "./../../scripts/build-if-package-version-different.sh"
+yarn workspaces foreach -pt -v --no-private exec "./../../scripts/build-if-package-version-different.sh"

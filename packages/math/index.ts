3847e748aaf0c6cb6c6f0c3f61f90ce73a0501b0
giavinh79 <giavinhlam@gmail.com>
2023-04-03 16:30:11 -0400

    add divide function

--- packages/math/index.ts
+++ packages/math/index.ts
@@ -6,6 +6,10 @@ export const subtract = (a: number, b: number): number => {
   return a - b;
 }
 
+export const divide = (a: number, b: number): number => {
+  return a / b;
+}
+
 export const multiply = (a: number, b: number) => {
   return a * b;
 }
